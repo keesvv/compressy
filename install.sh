5030a2ef97d92daa8c27454b11cab30aa4ca47d7
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-10-27 13:36:03 +0200

    Added installer

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo -e "\e[95mInstalling Compressy...\e[0m"
+echo "Installing required dependencies..."
+pip3 install colored terminaltables
+echo -e "\e[92mSuccessfully installed all dependencies!\e[0m"
+echo "Setting execute permissions..."
+chmod +x ./compressy.py
+
+echo -e "You may now run \e[1m./compressy.py\e[0m."
