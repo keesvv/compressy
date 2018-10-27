63f4d3956ef6bd8411cce82d4ff085a69fafd212
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-10-27 13:37:34 +0200

    Small refactoring

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 echo -e "\e[95mInstalling Compressy...\e[0m"
+
+# Install dependencies
 echo "Installing required dependencies..."
 pip3 install colored terminaltables
 echo -e "\e[92mSuccessfully installed all dependencies!\e[0m"
+
+# Set execute permissions
 echo "Setting execute permissions..."
 chmod +x ./compressy.py
 
