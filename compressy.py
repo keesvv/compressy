82ec8dd6af68f8e4b10efd5d251cfa7639b4d387
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-10-27 14:08:28 +0200

    Added support for gzip

--- compressy.py
+++ compressy.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import lzma
 import sys
 import os.path
 from colored import fg, bg, attr
@@ -19,32 +18,33 @@ class TableFormat:
         return ["%sTotal%s" % (attr(1), attr(0)),
             "", "", "%s %s %s bytes" % (bg("blue") + fg("white"), totalBenefit, attr(0))]
 
-def compressFile(file):
-    # Validate file path
-    if os.path.isfile(file) != True:
-        print(fg("red") + "Error: %s does not exist." % file + attr(0))
-        return
+class Compression:
+    def compressFile(file):
+        # Validate file path
+        if os.path.isfile(file) != True:
+            print(fg("red") + "Error: %s does not exist." % file + attr(0))
+            return
 
-    content = open(file, "rb").read()
-    contentCompressed = lzma.compress(content)
-    benefit = len(content) - len(contentCompressed)
+        content = open(file, "rb").read()
+        contentCompressed = compress.compress(content)
+        benefit = len(content) - len(contentCompressed)
 
-    tableData.append(TableFormat.getFormat(
-        file, content, contentCompressed, benefit))
+        tableData.append(TableFormat.getFormat(
+            file, content, contentCompressed, benefit))
 
-    global totalBenefit
-    totalBenefit = totalBenefit + benefit
+        global totalBenefit
+        totalBenefit = totalBenefit + benefit
 
-    # Write to file
-    with lzma.open(file + ".xz", "w") as out:
-        out.write(content)
+        # Write to file
+        with compress.open(file + "." + ext, "w") as out:
+            out.write(content)
 
 def checkArgs():
     # Check if user has entered arguments
     if len(sys.argv) == 1:
         print("Please enter the name of the files to compress.\n" +
             attr(1) + "\tExample: %s %sexample.txt" % (sys.argv[0], fg("green")) + attr(0))
-        exit()    
+        exit()
 
 checkArgs()
 
@@ -57,11 +57,21 @@ tableData = [
 files = sys.argv
 files.pop(0)
 
+if files[0] == "--gzip":
+    files.pop(0)
+    compression = "gzip"
+    ext = "gz"
+    import gzip as compress
+else:
+    compression = "lzma"
+    ext = "xz"
+    import lzma as compress
+
 print("Compressing %i file%s..."
     % (len(files), "s" if len(files) > 1 else ""))
 
 for file in files:
-    compressFile(file)
+    Compression.compressFile(file)
 
 tableData.append(TableFormat.getTotalFormat(totalBenefit))
 
